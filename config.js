e87db8d0b17e0cb67796b39890296b3e12138572
kjdhru46 <144645709+kjdhru46@users.noreply.github.com>
2023-09-19 10:51:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,19 +3,18 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "923184070915"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
+global.owner = process.env.OWNER_NUMBER || "15194881878"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://jonoliu676:jonoliu676@cluster0.nfnypnn.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'xheikhsalman4422@gmail.com'
 global.github = 'https://github.com/salmanytofficial/XLICON-MD'
-global.location = 'Lahore Pak'
+global.location = 'Sri Lanka'
 global.gurl = 'https://instagram.com/itz_shaikho/' // add your username
-global.sudo = process.env.SUDO || '923184070915' 
-global.devs = '923184070915';
-global.website = 'https://github.com/salmanytofficial/XLICON-MD' //wa.me/+91000000000000
+global.sudo = process.env.SUDO || '15194881878' 
+global.devs = '15194881878';
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
 module.exports = {
-  sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
+  sessionName:  process.env.SESSION_ID ||  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUNESmNZay9BL2pHbGlraElEMWxZSjN6UC81YUxhMEFTNTNLWm8reDJVcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN1pmK2RaeDg3VnRjMkE1ZGRCQ3BRb2Z1bGZUUERTRXhLUWZuRndNay9HTT0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJVQ2Y5TnB5NXhvOE5HSXcxd09kSFNxaWhXNTcveWg1NGhNYkpqUGEyazNNPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrMkU1MHFSMk5RLzVjdkJXUHhRd3JtejRxWW5iemsweWFNa1hDaStRSGdNPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndNT3VWQ29pMU1BeTRNRmU2OXRZdE00NWE5NWlya2hyYlJwMDBxRC93MzA9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkhTejUyQmFxNmpaS2xWakRwN2Iwd2lwbFFPVzByUmZib0ZNc3BaQi83VDA9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhS0lPZ0k5RDd1LzBZaTBtR2RHSXh4QnFzMjJJb3NMc3NYYlB6NlhhOTlDUW1CbVhHN0cyTHBNQzVzdFpIMklKa2ZEZ2ZGbzY0VnBDKy82Q0oxYVNEUT09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjM1LCJhZHZTZWNyZXRLZXkiOiJjajBWaDFVTkw5VlRhUzk2TEZFYldQbWRic2J2VmowYTFpakhTNUhoTnN3PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIzZFQzQzN2V1NCMlV3bUxEeC05NWd3IiwicGhvbmVJZCI6IjgzZGJiZjZlLTNiYzEtNDhhZi1iODc0LTlhNDcyZmNlNWQzMyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ2aGxiYTJnMDEwaVBHMWlvL1FTeHVhaWhhOGM9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZkN1IzdXVoMTdwMmhTWjBmZWNmakJhajV2TT0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0pEWnd1NEVFTWJlcEtnR0dBRT0iLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiajBteXVBbzZFeW04dzFWNUN6bzdzVzM2SmdmWmZrRE0xd20yVzRUNjZVdz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiVGlkZVZHVlZ0NnVqUnFPeVA0dXgvZ3pWdlIzQXFYSzk1NExzakllRndzWlduSGJoNnF2eWlERm9peGxvMnFaMG5ZbEY3bXNmcldtN0FjeklmNVgvQUE9PSIsImRldmljZVNpZ25hdHVyZSI6ImZDeXlsalc4VC9yWlhNZkw0TFg0aWRJR2Z4VWhtNHk3WHFhb3lOMUI0SkY4NFNVd1p2bndSNVhNZnhPZ3k0TER1Z3VSdzByWWMyc3RHd29tRHg4aEFBPT0ifSwibWUiOnsiaWQiOiIxNTE5NDg4MTg3ODoxN0BzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJKdSBIYW4g8J+lsCJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIxNTE5NDg4MTg3ODoxN0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJZOUpzcmdLT2hNcHZNTlZlUXM2TzdGdCtpWUgyWDVBek5jSnRsdUUrdWxNIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNjk1MTAwNzQ1LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUdwdyJ9",  //PUT Session Id Here, 
   author:  process.env.PACK_AUTHER ||  'XLICON BOT',
   packname:  process.env.PACK_NAME || 'MADE BY SALMAN AHMAD',
    
@@ -25,7 +24,7 @@ module.exports = {
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '94' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
